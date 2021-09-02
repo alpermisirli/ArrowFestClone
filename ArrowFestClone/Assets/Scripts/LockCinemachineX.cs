0b4b0f1b47e6e18ccf64de65f811e76655a57823
RedLegend97 <alpermisirli@gmail.com>
2021-09-02 13:21:22 +0300

    Camera movement

--- ArrowFestClone/Assets/Scripts/LockCinemachineX.cs
+++ ArrowFestClone/Assets/Scripts/LockCinemachineX.cs
@@ -6,7 +6,7 @@ using Cinemachine;
 /// </summary>
 public class LockCinemachineX : CinemachineExtension
 {
-    [Tooltip("Lock the camera's Z position to this value")]
+    [Tooltip("Lock the camera's X position to this value")]
     public float m_XPosition = 0;
 
     protected override void PostPipelineStageCallback(

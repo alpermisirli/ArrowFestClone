41c93682b24d33b38ff6dd1a81baddd7c0ef006f
RedLegend97 <61790422+RedLegend97@users.noreply.github.com>
2021-09-06 17:06:01 +0300

    Add basic game manager

--- ArrowFestClone/Assets/Scripts/GameManager.cs
+++ ArrowFestClone/Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject arrowStack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameOverCheck();
+    }
+
+    private void GameOverCheck()
+    {
+        if (arrowStack.transform.childCount <= 0)
+        {
+            Debug.Log("Game must be over");
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        //TODO MAKE GAME OVER SEQUENCE BETTER
+        SceneManager.LoadScene(0);
     }
 }

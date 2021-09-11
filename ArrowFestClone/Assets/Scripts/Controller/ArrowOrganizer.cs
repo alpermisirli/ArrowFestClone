d624af7d4d493208c6f5d9ecb83d31d4aa0faa2a
RedLegend97 <61790422+RedLegend97@users.noreply.github.com>
2021-09-11 17:37:27 +0300

    Project save
    Needed to go out just a quick save

--- ArrowFestClone/Assets/Scripts/Controller/ArrowOrganizer.cs
+++ ArrowFestClone/Assets/Scripts/Controller/ArrowOrganizer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,15 @@ public class ArrowOrganizer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        int count = transform.childCount;
+        for(int i = 0; i < count; i++)
+        {
+            Transform child = transform.GetChild(i);
+            //TODO THE POSITION OF EACH CHILDREN
+        }
     }
 }

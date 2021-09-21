23c9fafa1029b60903a8d41fdc6b94b09f7b778f
RedLegend97 <61790422+RedLegend97@users.noreply.github.com>
2021-09-21 17:39:30 +0300

    Revert "Added dynmaic spiral shape calculation for arrows"
    This reverts commit ce717c0a0e90e6b6d2468a56d6d8b5ccd9899ade.

--- ArrowFestClone/Assets/Scripts/Controller/ArrowOrganizer.cs
+++ ArrowFestClone/Assets/Scripts/Controller/ArrowOrganizer.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 
 public class ArrowOrganizer : MonoBehaviour
 {
-    [SerializeField] private float rx = 2f; // horizontal radius
-    [SerializeField] private float ry = 1.5f; // vertical radius
+    //TODO PUT TO THE ARROWSTACK GAMEOBJECT TO ORDER THE CHILDREN ARROW IN A WAY
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
 
     private void Update()
     {
-        ArrowPositionUpdater();
-    }
-
-    private void ArrowPositionUpdater()
-    {
-        int arrowCount = transform.childCount;
-        float angleSelection = Mathf.PI * 2f / arrowCount;
-        for (int i = 0; i < arrowCount; i++)
+        int count = transform.childCount;
+        for(int i = 0; i < count; i++)
         {
             Transform child = transform.GetChild(i);
-            float angle = i * angleSelection;
-            Vector3 arrowPosition = transform.position + new Vector3(rx * Mathf.Cos(angle),
-                ry * Mathf.Sin(angle) * Mathf.Sqrt(i),
-                0f);
-            child.position = arrowPosition;
+            //TODO THE POSITION OF EACH CHILDREN
         }
     }
 }
